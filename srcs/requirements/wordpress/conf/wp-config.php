0d408955d073f5b29b8c1bf23d9e4eb33ceac9a5
Ouhammou1 <brahimouhammou2022@gmail.com>
2025-12-07 13:57:23 +0100

    Inception

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_NAME' , getenv("MYSQL_DATABASE"));
+define('DB_USER', getenv('MYSQL_USER'));
+define('DB_PASSWORD' , getenv('MYSQL_PASSWORD'));
+define('DB_HOST' , getenv('MYSQL_HOST'));
+
+
+define('DB_CHARSET' , 'utf8');
+define('DB_COLLATE' '');
+
+$table_prefix= 'wp_';
+define('WP_DEBUG' , true);
+
+
+if( ! define('ABSPATH') )
+    define('ABSPATH' , __DIR__ . '/');
+
+
+require_once ABSPATH . 'wp-config.php';
